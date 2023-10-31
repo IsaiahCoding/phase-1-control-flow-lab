60fa1f3b9d9b2a2e9e769004d4368f96124e8066
Isaiah <IaguileraCFE@gmail.com>
2023-10-31 08:48:31 -0700

    Isaiah A Flow LAb

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride) {
+  if (ride <= 400) {
+    return "This one is on me!";
+  } else if (ride > 400 && ride <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (ride > 2500) {
+    return "No can do.";
+  } else {
+    return "I will gladly take your thirty bucks.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  let message;
+  if (tip === "generous") {
+    message = "Thank you so much.";
+  } else if (tip === "not as generous") {
+    message = "Thank you.";
+  } else {
+    message = "Bye.";
+  }
+  return message;
 }
